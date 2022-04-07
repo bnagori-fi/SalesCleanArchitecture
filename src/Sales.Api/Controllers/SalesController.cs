aed41d7ae72d602c5b19bc5be2be12eb7c14c372
Hardys Perez Bermudez <60011073+hardys-perez@users.noreply.github.com>
2022-04-07 12:55:00 -0700

    Create Sales.Api.Test project Add a test class for SalesController

--- src/Sales.Api/Controllers/SalesController.cs
+++ src/Sales.Api/Controllers/SalesController.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sales.Application.Features.Sales.Command.UpdateSales;
 using Sales.Application.Features.Sales.Queries.GetSalesByRegion;
